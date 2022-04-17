1feba4ae6e4877008940128b5f73e233a68ea9e9
bassel-mousa <--get>
2022-04-17 22:42:31 +0300

    Making Animal Table

---
+++ database/migrations/2022_04_17_193942_create_animals_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('animals', function (Blueprint $table) {
+            $table->id();
+            $table->uuid('file_serial_number');
+            $table->string('image')->nullable();
+            $table->string('name');
+            $table->string('type');
+            $table->string('animal_id')->unique();
+            $table->date('birth_date');
+            $table->string('gender');
+            $table->foreignId('user_id')->constrained('users')->cascadeOnDelete()->cascadeOnUpdate();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('animals');
+    }
+};
