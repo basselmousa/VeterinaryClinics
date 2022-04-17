12a0556463249fe69a4333f485e1ff8a0dae52b9
bassel-mousa <--get>
2022-04-17 22:39:24 +0300

    make Doctor Dates Table

---
+++ database/migrations/2022_04_17_012333_create_doctor_dates_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('doctor_dates', function (Blueprint $table) {
+            $table->id();
+            $table->string('type');
+            $table->string('day');
+            $table->time('time');
+            $table->decimal('price', 5,2);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('doctor_dates');
+    }
+};
