9906e9c5144c10a77d4d519a47e527cebb1e911d
bassel-mousa <--get>
2022-05-21 01:22:01 +0300

    Making Static Configs

---
+++ app/Http/Configs.php
@@ -0,0 +1,32 @@
+<?php
+
+
+namespace App\Http;
+
+
+class Configs
+{
+
+    public static $cities = [
+        'Amman','Zarqa','Irbid','ar-Rusaifa','al-Quwaisima','Wadi as-Sir','Tila al-Ali','Churaibat as-Suq','Aqaba',
+        'as-Salt','ar-Ramtha','Sakib','Schafa Badran',"Na'ur","Kurayyima","Azmi al-Mufti","Gawr asch-Schafi",
+        'Andschara',"Bait Ras","al-Maschariqa","Aidun","as-Sarih","al-Karak","al-Husun","Kufrindscha","at-Tafila","Askan Abu Nusair",
+        "al-Hasimiya","Ma'an","ad-Dulail","Mardsch al-Hamam","Jerash","Umm qaseer","Ain al-Bascha","Muchayyam Hetten","Sahab"
+    ];
+
+    public static $types = [
+        'Clinic',
+        'Home'
+    ];
+
+    public static $days = [
+        'Sunday',
+        'Monday',
+        'Tuesday',
+        'Wednesday',
+        'Thursday',
+        'Friday',
+        'Saturday'
+    ];
+
+}
