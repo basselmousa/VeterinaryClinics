9701bd74e0ebd20ef75446d443f63a75b314593b
bassel-mousa <--get>
2022-05-21 01:24:00 +0300

    Add Some Relations

--- app/Models/Doctor.php
+++ app/Models/Doctor.php
@@ -4,8 +4,26 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Foundation\Auth\User as Authenticatable;
 
-class Doctor extends Model
+class Doctor extends Authenticatable
 {
     use HasFactory;
+    protected $guarded = [];
+
+    public function certifications()
+    {
+        return $this->hasMany(Certificate::class);
+    }
+
+    public function dates()
+    {
+        return $this->hasMany(DoctorDate::class, 'doctor_id');
+    }
+
+    public function rate()
+    {
+        return $this->belongsToMany(User::class, 'rates');
+    }
+
 }
--- app/Models/DoctorDate.php
+++ app/Models/DoctorDate.php
@@ -8,4 +8,5 @@ use Illuminate\Database\Eloquent\Model;
 class DoctorDate extends Model
 {
     use HasFactory;
+    protected $guarded = [];
 }
