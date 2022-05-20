9d466e16e5ebee1dfd18a8f354c4d45215e206d5
bassel-mousa <--get>
2022-05-21 01:22:02 +0300

    Some Changes To show data

--- resources/views/doctor/layouts/sidebar.blade.php
+++ resources/views/doctor/layouts/sidebar.blade.php
@@ -1,8 +1,8 @@
 <!-- Main Sidebar Container -->
 <aside class="main-sidebar sidebar-dark-primary elevation-4">
     <!-- Brand Logo -->
-    <a href="index3.html" class="brand-link">
-        <img src="dist/img/AdminLTELogo.png" alt="AdminLTE Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
+    <a href="#" class="brand-link">
+        <img src="{{ asset('admin/images/logo.png') }}" alt="AdminLTE Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
         <span class="brand-text font-weight-light">Veterinary Clinics</span>
     </a>
 
@@ -11,10 +11,24 @@
         <!-- Sidebar user panel (optional) -->
         <div class="user-panel mt-3 pb-3 mb-3 d-flex">
             <div class="image">
-                <img src="dist/img/user2-160x160.jpg" class="img-circle elevation-2" alt="User Image">
+                <img src="{{ asset('storage/'.auth('doctor')->user()->image) }}" class="img-circle elevation-2" alt="User Image">
             </div>
             <div class="info">
-                <a href="#" class="d-block">Alexander Pierce</a>
+                <div class="row">
+                    <div class="col-md-10">
+                        <a href="{{ route('dashboard.doctor.profile.profile') }}" class="d-block">{{ auth('doctor')->user()->full_name }} </a>
+                    </div>
+                    <div class="col-md-2">
+                        <i class="fa fa-door-open right mr-3 " style="color:  white; cursor: pointer" onclick="event.preventDefault();
+                           document.getElementById('logout-form').submit()"></i>
+
+                        <form id="logout-form" action="{{ route('doctors.logout') }}" method="post" class="d-none">
+                            @csrf
+
+                        </form>
+                    </div>
+                </div>
+
             </div>
         </div>
 
@@ -25,7 +39,7 @@
                 <!-- Add icons to the links using the .nav-icon class
                      with font-awesome or any other icon font library -->
                 <li class="nav-item">
-                    <a href="pages/widgets.html" class="nav-link">
+                    <a href="{{ route('dashboard.doctor.home') }}" class="nav-link">
                         <i class="nav-icon fas fa-th"></i>
                         <p>
                             Dashboard
@@ -33,7 +47,7 @@
                     </a>
                 </li>
                 <li class="nav-item">
-                    <a href="pages/widgets.html" class="nav-link">
+                    <a href="{{ route('dashboard.doctor.reports') }}" class="nav-link">
                         <i class="nav-icon fas fa-th"></i>
                         <p>
                             Reports
@@ -52,19 +66,19 @@
                     </a>
                     <ul class="nav nav-treeview">
                         <li class="nav-item">
-                            <a href="pages/charts/chartjs.html" class="nav-link">
+                            <a href="#" class="nav-link">
                                 <i class="far fa-circle nav-icon"></i>
                                 <p>Pending Appointments</p>
                             </a>
                         </li>
                         <li class="nav-item">
-                            <a href="pages/charts/flot.html" class="nav-link">
+                            <a href="#" class="nav-link">
                                 <i class="far fa-circle nav-icon"></i>
                                 <p>Clinic Appointments</p>
                             </a>
                         </li>
                         <li class="nav-item">
-                            <a href="pages/charts/inline.html" class="nav-link">
+                            <a href="#" class="nav-link">
                                 <i class="far fa-circle nav-icon"></i>
                                 <p>Home Appointments</p>
                             </a>
@@ -73,7 +87,7 @@
                 </li>
                 <li class="nav-item">
                     <a href="#" class="nav-link">
-                        <i class="nav-icon fas fa-tree"></i>
+                        <i class="nav-icon fa fa-gears"></i>
                         <p>
                             Settings
                             <i class="fas fa-angle-left right"></i>
@@ -81,13 +95,13 @@
                     </a>
                     <ul class="nav nav-treeview">
                         <li class="nav-item">
-                            <a href="pages/UI/general.html" class="nav-link">
+                            <a href="{{ route('dashboard.doctor.dates.index') }}" class="nav-link">
                                 <i class="far fa-circle nav-icon"></i>
                                 <p>Dates</p>
                             </a>
                         </li>
                         <li class="nav-item">
-                            <a href="pages/UI/icons.html" class="nav-link">
+                            <a href="{{ route('dashboard.doctor.certificates.index') }}" class="nav-link">
                                 <i class="far fa-circle nav-icon"></i>
                                 <p>Certificates</p>
                             </a>
@@ -97,7 +111,13 @@
                 </li>
 
             </ul>
+
+
         </nav>
+
+
+
+
         <!-- /.sidebar-menu -->
     </div>
     <!-- /.sidebar -->
