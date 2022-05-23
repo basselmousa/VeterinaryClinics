0d250c821387077eb063b2f3c92a2c06734fe42d
bassel-mousa <--get>
2022-05-23 21:36:54 +0300

    Adding Pivot Model To Make Get Info Easy

---
+++ app/Models/Appointment.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Relations\Pivot;
+
+class Appointment extends Pivot
+{
+    //
+
+    protected $table = 'appointments';
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function doctor()
+    {
+        return $this->belongsTo(Doctor::class);
+    }
+
+    public function animal()
+    {
+        return $this->belongsTo(Animal::class);
+    }
+}
